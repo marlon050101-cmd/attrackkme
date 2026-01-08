03dcd8b52f8043c92ab3871d0e4365afc6caa693
angelocallocallo1111@gmail.com <angelocallocallo1111@gmail.com>
2026-01-08 09:10:01 +0800

    meowcommit

--- Attrak/Services/ApiConfig.cs
+++ Attrak/Services/ApiConfig.cs
@@ -3,7 +3,7 @@ namespace Attrak.Services
     public static class ApiConfig
     {
         // Change this URL for different environments
-        public static string BaseUrl { get; set; } = "https://attrak-8gku.onrender.com/";
+        public static string BaseUrl { get; set; } = "https://attrack.onrender.com/";
         
         // Get full API URL for an endpoint
        
